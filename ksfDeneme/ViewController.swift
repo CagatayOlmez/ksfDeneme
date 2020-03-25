c27695874d9a438ed178dfc45a2b15c885eb002d
CagatayOlmez <cagatayolmez99@gmail.com>
2020-03-25 12:38:57 +0300

    first commit

---
+++ ksfDeneme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ksfDeneme
+//
+//  Created by apple on 25.03.2020.
+//  Copyright © 2020 cagatay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
